04cbeff8a18d1a77201835d2d104ad3b897e447a
Bohdanok <bohdan.milian@gmail.com>
2025-05-19 13:02:58 +0300

    Typing element fix

--- projecttypist/src/components/Typing/TypingElement/TypingElement.jsx
+++ projecttypist/src/components/Typing/TypingElement/TypingElement.jsx
@@ -20,16 +20,18 @@ function TypingElement() {
     const applyInputValue = useCallback((newInputValue) => {
         if (!newInputValue) return
         const parsedValue = parseInt(newInputValue)
-        if (isNaN(parsedValue)) {
-            setWrongInputText("Wrong input")
+        if (isNaN(parsedValue) || !(/^-?\d+(\.\d+)?$/.test(newInputValue))) {
+            setWrongInputText("Wrong input!")
             return
         }
         if (parsedValue > 10000) {
-            setWrongInputText("input value is larget than 10000")
+            setWrongInputText("Input value is larget than 10000!")
+            // alert("Input value is larget than 10000!");
             return
         }
         if (parsedValue < 1) {
-            setWrongInputText("input value is smaller than 1")
+            setWrongInputText("Input value is smaller than 1!")
+            // alert("Input value is smaller than 1!")
             return
         }
         modeValues.setValue(parsedValue)
@@ -71,14 +73,17 @@ function TypingElement() {
 
 
     return <>
+        <div className="modes-container">
         <div className="modes-manager">
             <button className={wordsClass + " button-type"} value="words" onClick={(e) => changeMode(e)}>words</button>
             <button className={timeClass + " button-type"} value="time" onClick={(e) => changeMode(e)}>time</button>
             <input className="mode-input" type="text" onChange={(e) => updateInputValue(e)} value={inputValue} />
+            <button className="button-type" onClick={() => typingInputRef.current.resetTypingInput()}>new text</button>
+        </div>
             <div className="wrongInputText">{wrongInputText}</div>
         </div>
 
-        <button className="button-type" onClick={() => typingInputRef.current.resetTypingInput()}>new text</button>
+
         <Timer refference={timerRef} timeLimit={getTimeValue()} typingInputRef={typingInputRef}></Timer>
         {/* <p className="time-text">time: {timeTyping}s</p> */}
 
